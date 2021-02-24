4bbd8f5300d53123452e88fddcaaa768be49199f
nicolasbraz <nicolas.braz@gmail.com>
2021-02-24 19:49:53 -0300

    Finalizando o garbageCollector

---
+++ GarbageCollector/gc1.rb
@@ -0,0 +1,21 @@
+class Teste 
+end
+
+t1 = Teste.new 
+t2 = Teste.new 
+t3 = Teste.new 
+
+count = ObjectSpace.each_object(Teste) do |object| 
+  puts object 
+end 
+
+puts "#{count} objectos encontrados"
+
+t2 = nil 
+GC.start 
+
+count = ObjectSpace.each_object(Teste) do |object|
+  puts object 
+end 
+
+puts "#{count} objetos encontrados."
