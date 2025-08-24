a62f8da9339d3ba15f574814041f14de7251881e
nicolasbraz <nicolas.braz@gmail.com>
2025-08-23 22:10:28 -0300

    Update gc1.rb

--- GarbageCollector/gc1.rb
+++ GarbageCollector/gc1.rb
@@ -1,11 +1,11 @@
-class Teste 
+class gc
 end
 
-t1 = Teste.new 
-t2 = Teste.new 
-t3 = Teste.new 
+t1 = gc.new 
+t2 = gc.new 
+t3 = gc.new 
 
-count = ObjectSpace.each_object(Teste) do |object| 
+count = ObjectSpace.each_object(gc) do |object| 
   puts object 
 end 
 
@@ -14,7 +14,7 @@ puts "#{count} objectos encontrados"
 t2 = nil 
 GC.start 
 
-count = ObjectSpace.each_object(Teste) do |object|
+count = ObjectSpace.each_object(gc) do |object|
   puts object 
 end 
 
