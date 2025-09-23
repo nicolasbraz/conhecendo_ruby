2bfd29a7dd2bebad83f52df00b632f43a0f18574
nicolasbraz <nicolas.braz@gmail.com>
2025-09-23 10:42:42 -0300

    Update bench1.rb

--- Benchmarks/bench1.rb
+++ Benchmarks/bench1.rb
@@ -3,7 +3,7 @@ require "parallel"
 
 Benchmark.bm do |bm|
   bm.report do 
-    Parallel.map 1..20, in_threads: 4 do |nr|
+    Parallel.map 1..30, in_threads: 4 do |nr|
       5.times {|t| sleep rand; }
     end
   end
