c89e8829d72f2522dbcce8ee368f99a8d0ec8689
nicolasbraz <nicolas.braz@gmail.com>
2025-10-26 22:12:44 -0300

    Change sleep iterations from 5 to 10

--- Benchmarks/bench1.rb
+++ Benchmarks/bench1.rb
@@ -4,7 +4,7 @@ require "parallel"
 Benchmark.bm do |bm|
   bm.report do 
     Parallel.map 1..30, in_threads: 4 do |nr|
-      5.times {|t| sleep rand; }
+      10.times {|t| sleep rand; }
     end
   end
 end
